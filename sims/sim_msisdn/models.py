578045dbce09095898831020260ae1c1bdc2ec4a
anpachecos <an.pachecos@duocuc.cl>
2024-11-10 12:39:11 -0300

    Comunicando sims con usuarios

--- sims/sim_msisdn/models.py
+++ sims/sim_msisdn/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import requests
+from django.conf import settings
+from .utils import obtener_datos_usuario  
+
 
 class EstadoSim(models.Model):
     nombre_estado = models.CharField(max_length=100)
@@ -19,13 +23,25 @@ class MSISDN(models.Model):
 
 class SimMsisdn(models.Model):
     iccid = models.CharField(max_length=20, unique=True)
-    id_msisdn = models.ForeignKey(MSISDN, on_delete=models.CASCADE)
+    id_msisdn = models.ForeignKey('MSISDN', on_delete=models.CASCADE)
     inicio_relacion = models.DateField()
     fin_relacion = models.DateField(null=True, blank=True)
     motivo_fin = models.CharField(max_length=20, blank=True, null=True)
-    id_estado = models.ForeignKey(EstadoSim, on_delete=models.CASCADE)
+    id_estado = models.ForeignKey('EstadoSim', on_delete=models.CASCADE)
     id_usuario = models.IntegerField() 
     fecha_creacion = models.DateField()
+    usuario_nombre = models.CharField(max_length=100, blank=True, null=True)
+    usuario_apellido = models.CharField(max_length=100, blank=True, null=True)
+    usuario_correo = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return self.iccid
+
+    def save(self, *args, **kwargs):
+        # Llama a la función para obtener datos del usuario
+        usuario_data = obtener_datos_usuario(self.id_usuario)
+        if usuario_data:
+            self.usuario_nombre = usuario_data.get('nombre')
+            self.usuario_apellido = usuario_data.get('apellido')
+            self.usuario_correo = usuario_data.get('correo')
+        super().save(*args, **kwargs)
--- sims/sim_msisdn/utils.py
+++ sims/sim_msisdn/utils.py
@@ -28,3 +28,17 @@ def obtener_todos():
             # Guarda la lista de usuarios en la caché por 5 minutos
             cache.set("todos_los_usuarios", usuarios, timeout=300)
     return usuarios
+
+
+def obtener_datos_usuario(id_usuario):
+    url = f'{settings.USUARIOS_AUTH_URL}/usuarios/usuarios/{id_usuario}/'  # Asegúrate de usar el puerto correcto
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: Usuario con id {id_usuario} no encontrado. Status code: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error al conectar con el servicio de usuarios: {e}")
+        return None
