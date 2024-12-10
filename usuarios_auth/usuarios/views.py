f693477836513049829765282016ca9636a9b2ca
RafaOteiza <133901503+RafaOteiza@users.noreply.github.com>
2024-12-10 14:03:02 -0300

    Actualización Final
    Actualización Final

--- usuarios_auth/usuarios/views.py
+++ usuarios_auth/usuarios/views.py
@@ -11,12 +11,18 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuarios.objects.all()
     serializer_class = UsuarioSerializer
 
-    # Personaliza el filtrado
     def get_queryset(self):
         queryset = super().get_queryset()
+        
+        # Filtrar por correo y contraseña si están presentes
         correo = self.request.query_params.get('correo', None)
         contraseña = self.request.query_params.get('contraseña', None)
-
         if correo and contraseña:
             queryset = queryset.filter(correo=correo, contraseña=contraseña)
+        
+        # Filtrar por ID si está presente
+        user_id = self.request.query_params.get('id', None)
+        if user_id:
+            queryset = queryset.filter(id=user_id)
+        
         return queryset
